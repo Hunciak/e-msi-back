907906d93e555d6fd8fc166c5862811ed54fbb23
Hunciak <michal.zelechowski@o2.pl>
2023-08-23 20:11:17 +0200

    corsik

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ import contractorsRouter from './routers/contractors.router.mjs';
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://141.95.67.150:3000',
     credentials: true,
 }));
 
